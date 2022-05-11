91d41067fddbcdeb5eebc688c6c245ad806e685c
humphreyokoth <humpasp@gmail.com>
2022-05-11 16:10:27 +0300

    alxJavaScript

--- alx.js
+++ alx.js
@@ -1,5 +1,9 @@
 
+//console.log("hiya friend!");
 
+// Code looping through numbers 0 to 9
+for (let i=0;i<10;i++){
+console.log([i]);
+}
 
-
-
+document.getElementsByTagName("h1")[0].style.color = "#ff0000";
